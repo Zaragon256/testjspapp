271f068f65557e36c56cfe96848982141447c356
Zaragon256 <luis.arenas.zu@gmail.com>
2023-02-13 16:57:10 -0500

    Initial commit

---
+++ src/main/java/com/zaragon256/testjspapp/TestjspappApplication.java
@@ -0,0 +1,13 @@
+package com.zaragon256.testjspapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestjspappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestjspappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaragon256/testjspapp/TestjspappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaragon256.testjspapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestjspappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
